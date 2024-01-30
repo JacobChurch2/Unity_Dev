cc1f48524dbfe673f5e41a6becbcf64d0ce84977
JacobChurch2 <jchurch@student.neumont.edu>
2024-01-30 15:23:32 -0700

    Spawner

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -7,9 +7,9 @@ using Unity.Mathematics;
 public class PathFollower : MonoBehaviour
 {
     [SerializeField] SplineContainer splineContainer;
-    [Range(0,40)] public float speed = 1;
+    [SerializeField, Range(0,1)] float tdistance = 0;
 
-    float tdistance = 0; //distance along spline (0-1)
+    [Range(0,40)] public float speed = 1;
 
     //public float speed { get; set; }
     public float length { get { return splineContainer.CalculateLength(); } }
