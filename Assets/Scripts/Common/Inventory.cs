9094260c9ae312a708f69cd7fdf20d159404a335
JacobChurch2 <jchurch@student.neumont.edu>
2024-02-01 15:18:32 -0700

    Rocket Launcher

---
+++ Assets/Scripts/Common/Inventory.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] Item[] items;
+    public Item currentItem {  get; private set; } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentItem = items[0];
+        currentItem.Equip();
+    }
+
+    public void Use()
+    {
+        currentItem?.Use();
+    }
+
+    public void StopUse()
+    {
+        currentItem?.StopUse();
+    }
+
+}
